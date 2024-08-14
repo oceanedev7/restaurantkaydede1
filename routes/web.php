7b8347e9e7d65996793b25643e1962e658fda7d5
oceanedev7 <oceanepy7@gmail.com>
2024-08-14 12:25:35 -0400

    authetification (creation du middleware, determination des roles, attribution des routes)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\Role;
 use App\Http\Controllers\AddProductController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProductViewController;
@@ -19,31 +20,19 @@ Route::get('/contact', function () {
     return view('pages.user.contact');
 })->name('contactez-nous');
 
-Route::get('/demandecontact', [ContactController::class, 'index']);
-Route::post('/demandedecontact', [ContactController::class, 'create'])->name('contact');
-
-Route::get('/addproduct', [AddProductController::class, 'index'])->name('afficherProduit');
-Route::post('/createproduct', [AddProductController::class, 'create'])->name('ajouterProduit');
-Route::get('/deleteproduct/{id}', [AddProductController::class, 'destroy'])->name('supprimerProduit');
-Route::get('/editproduct/{id}', [AddProductController::class, 'edit'])->name('editProduit');
-Route::post('/updateproduct/{id}', [AddProductController::class, 'update'])->name('updateProduit');
-
 Route::get('/notrecarte', [ProductViewController::class, 'index'])->name('notrecarte');
 Route::get('/produit/{id}', [ProductViewController::class, 'show'])->name('produit');
-
+Route::post('/demandedecontact', [ContactController::class, 'create'])->name('contact');
 
 Route::get('/reserver', function () {
     return view('pages.user.reserver');
 })->name('reserver');
 
-Route::get('/nouvellereservation', [ReservationController::class, 'index'])->name('newresa');
 Route::post('/reserver', [ReservationController::class, 'create'])->name('reserver');
 
 Route::get('/authentification', function () {
     return view('auth.authentification');
 });
-
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -53,6 +42,21 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['role:admin'])->group(function () {
+Route::get('/nouvellereservation', [ReservationController::class, 'index'])->name('newresa');
+Route::get('/addproduct', [AddProductController::class, 'index'])->name('afficherProduit');
+Route::post('/createproduct', [AddProductController::class, 'create'])->name('ajouterProduit');
+Route::get('/deleteproduct/{id}', [AddProductController::class, 'destroy'])->name('supprimerProduit');
+Route::get('/editproduct/{id}', [AddProductController::class, 'edit'])->name('editProduit');
+Route::post('/updateproduct/{id}', [AddProductController::class, 'update'])->name('updateProduit');
+Route::get('/demandecontact', [ContactController::class, 'index']);
+
+});
+
+Route::middleware(['role:user'])->group(function () {
+    //
 });
 
 require __DIR__.'/auth.php';
